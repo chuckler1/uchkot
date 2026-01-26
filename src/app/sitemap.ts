0c6b430135f66a9172b082c7dcfdf1c8d0fb1d09
chuckler1 <denisdesdoz@yandex.ru>
2026-01-26 19:48:23 +0300

    seo: add og and sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://u4kot.ru/",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
+
